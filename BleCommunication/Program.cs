8e27901302a64bc91ab718ce670058a878eeed4d
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-05-28 06:33:06 +0200

    get advertising manager

--- BleCommunication/Program.cs
+++ BleCommunication/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BleServer.Infrastructure.BlueZ;
+using Tmds.DBus;
 
 namespace BleCommunication
 {
@@ -7,6 +9,19 @@ namespace BleCommunication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Bluetooth");
+
+            GetAdapterAsync();
+            var advertisingManager = GetAdvertisingManager();
+        }
+
+        private static IAdapter1 GetAdapterAsync()
+        {
+            return Connection.System.CreateProxy<IAdapter1>("org.bluez", "/org/bluez/hci0");
+        }
+
+        private static ILEAdvertisingManager1 GetAdvertisingManager()
+        {
+            return Connection.System.CreateProxy<ILEAdvertisingManager1>("org.bluez", "/org/bluez/hci0");
         }
     }
 }
