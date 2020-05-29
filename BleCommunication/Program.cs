50b9d995dc15f2b34fcd552978aff579fa144335
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-05-29 06:49:20 +0200

    Try publishing example advertisement

--- BleCommunication/Program.cs
+++ BleCommunication/Program.cs
@@ -1,22 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 using BleServer.Infrastructure.BlueZ;
 using Tmds.DBus;
 
 namespace BleCommunication
 {
-    class Program
+    public class ExampleAdvertisement : IDBusObject
     {
-        static void Main(string[] args)
+        public string Type { get; set; }
+        public string[] ServiceUUIDs { get; set; }
+        public IDictionary<string, object> ManufacturerData { get; set; }
+        public string[] SolicitUUIDs { get; set; }
+
+        public IDictionary<string, object> ServiceData { get; set; }
+
+        public bool IncludeTxPower { get; set; }
+
+        public string LocalName { get; set; }
+        public ObjectPath ObjectPath { get; set; }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello Bluetooth");
 
-            GetAdapterAsync();
+            var advertisement = CreateAdvertisement();
             var advertisingManager = GetAdvertisingManager();
+
+            advertisingManager.RegisterAdvertisementAsync(advertisement.ObjectPath, new Dictionary<string, object>());
         }
 
-        private static IAdapter1 GetAdapterAsync()
+        private static IDBusObject CreateAdvertisement()
         {
-            return Connection.System.CreateProxy<IAdapter1>("org.bluez", "/org/bluez/hci0");
+            var advertisement = new ExampleAdvertisement
+            {
+                ObjectPath = new ObjectPath("/org/bluez/example/advertisement"),
+                Type = "peripheral",
+                ServiceUUIDs = new[] {"180D", "180F"},
+                ManufacturerData = {{"0xffff", new[] {"0x00", "0x01", "0x02", "0x03"}}},
+                ServiceData = {{"9999", new[] {"0x00", "0x01", "0x02", "0x03"}}},
+                IncludeTxPower = true,
+                LocalName = "TestAdvertisement"
+            };
+
+            Connection.System.RegisterObjectAsync(advertisement);
+
+            return advertisement;
         }
 
         private static ILEAdvertisingManager1 GetAdvertisingManager()
