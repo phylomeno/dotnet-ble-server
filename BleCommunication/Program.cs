51e889bb3e37f83945efca710fa94ec7462174e3
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-05-30 11:52:12 +0200

    exclude fields not yet parseable

--- BleCommunication/Program.cs
+++ BleCommunication/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Common;
-using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using BleServer.Infrastructure.BlueZ;
@@ -10,7 +8,7 @@ using Tmds.DBus;
 namespace BleCommunication
 {
     [DBusInterface("org.bluez.LEAdvertisement1")]
-    interface ILEAdvertisement1 : IDBusObject
+    internal interface ILEAdvertisement1 : IDBusObject
     {
         Task ReleaseAsync();
         Task<object> GetAsync(string prop);
@@ -21,21 +19,15 @@ namespace BleCommunication
 
     public class Advertisement : ILEAdvertisement1
     {
-        public static readonly ObjectPath Path = new ObjectPath("/org/bluez/example/advertisement");
-        private IDictionary<string, object> _properties;
+        public static readonly ObjectPath Path = new ObjectPath("/org/bluez/example/advertisement0");
+        private readonly IDictionary<string, object> _properties;
+
         public Advertisement(IDictionary<string, object> properties)
         {
             _properties = properties;
         }
-        public ObjectPath ObjectPath
-        {
-            get
-            {
-                return Path;
-            }
-        }
 
-        public event Action<PropertyChanges> OnPropertiesChanged;
+        public ObjectPath ObjectPath => Path;
 
         public Task<IDictionary<string, object>> GetAllAsync()
         {
@@ -58,10 +50,13 @@ namespace BleCommunication
         {
             return SignalWatcher.AddAsync(this, nameof(OnPropertiesChanged), handler);
         }
+
         public Task ReleaseAsync()
         {
             throw new NotImplementedException();
         }
+
+        public event Action<PropertyChanges> OnPropertiesChanged;
     }
 
     internal class Program
@@ -79,41 +74,39 @@ namespace BleCommunication
                     Console.WriteLine("Advertisement created");
                     var advertisingManager = GetAdvertisingManager(connection);
 
-                    await advertisingManager.RegisterAdvertisementAsync(advertisement.ObjectPath, new Dictionary<string, object>());
+                    await advertisingManager.RegisterAdvertisementAsync(advertisement.ObjectPath,
+                        new Dictionary<string, object>());
                     Console.WriteLine("Advertisement registered");
-
                 }
             }).Wait();
 
 
             Console.WriteLine("Waiting");
-            while (true)
-            {
-                Thread.Sleep(5000);
-            }
+            while (true) Thread.Sleep(5000);
         }
 
         private static async Task<IDBusObject> CreateAdvertisement(Connection connection)
         {
-            var advertisement = new Advertisement(new Dictionary<string, object>()
+            var advertisement = new Advertisement(new Dictionary<string, object>
             {
                 {"Type", "peripheral"},
                 {"ServiceUUIDs", new[] {"180D", "180F"}},
-                {
-                    "ManufacturerData",
-                    new Dictionary<string, object> {{"0xffff", new[] {"0x00", "0x01", "0x02", "0x03"}}}
-                },
-                {"ServiceData", new Dictionary<string, object> {{"9999", new[] {"0x00", "0x01", "0x02", "0x03"}}}},
-                {"IncludeTxPower", true},
-                {"LocalName", "TestAdvertisement"}
+                // {
+                //     "ManufacturerData",
+                //     new Dictionary<string, object> {{"0xffff", new[] {"0x00", "0x01", "0x02", "0x03"}}}
+                // },
+                {"SolicitUUIDs", new[] {"180D", "180F"}},
+                // {"ServiceData", new Dictionary<string, object> {{"9999", new[] {"0x00", "0x01", "0x02", "0x03"}}}},
+                {"Includes", new string[0]},
+                {"LocalName", "Edelweiss ASD"},
+                // {"Appearance", 0},
+                {"Duration", (ushort) 10},
+                // {"Timeout", 1000}
             });
 
-            var advertisementProxy = connection.CreateProxy<ILEAdvertisement1>("org.bluez", advertisement.ObjectPath);
             await connection.RegisterObjectAsync(advertisement);
 
-            //await advertisementProxy.SetAsync("LocalName", "My Adv");
-
-            return advertisementProxy;
+            return advertisement;
         }
 
         private static ILEAdvertisingManager1 GetAdvertisingManager(Connection connection)
