5117352a47a51dda46cc8c6a78404dc26b363b1e
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-05-31 11:56:28 +0200

    add GattCharacteristic

---
+++ BleCommunication/Infrastructure/BlueZ/Gatt/GattCharacteristic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BleServer.Infrastructure.BlueZ.Core;
+
+namespace BleServer.Infrastructure.BlueZ.Gatt
+{
+    internal class GattCharacteristic : PropertiesBase<GattCharacteristic1Properties>, IGattCharacteristic1
+    {
+        public GattCharacteristic(GattCharacteristic1Properties properties) : base(properties)
+        {
+        }
+
+        public Task<byte[]> ReadValueAsync(IDictionary<string, object> Options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task WriteValueAsync(byte[] Value, IDictionary<string, object> Options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StartNotifyAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StopNotifyAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
