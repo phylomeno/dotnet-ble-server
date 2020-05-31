ea35e3c99494de98c9ce64f931c13dbdef9d1039
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-05-31 11:57:49 +0200

    add GattService class

---
+++ BleCommunication/Infrastructure/BlueZ/Gatt/GattService.cs
@@ -0,0 +1,11 @@
+﻿using BleServer.Infrastructure.BlueZ.Core;
+
+namespace BleServer.Infrastructure.BlueZ.Gatt
+{
+    internal class GattService : PropertiesBase<GattService1Properties>, IGattService1
+    {
+        public GattService(GattService1Properties properties) : base(properties)
+        {
+        }
+    }
+}
