fc782d305f0fed4856090bd33c7dd9b03d451495
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-05-31 08:31:26 +0200

    extract PropertiesBase class

---
+++ BleCommunication/Infrastructure/BlueZ/Core/PropertiesBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using BleServer.Infrastructure.BlueZ.Utilities;
+using Tmds.DBus;
+
+namespace BleServer.Infrastructure.BlueZ.Core
+{
+    public abstract class PropertiesBase<TV>
+    {
+        protected readonly TV Properties;
+
+        protected PropertiesBase(TV properties)
+        {
+            Properties = properties;
+        }
+
+        public ObjectPath ObjectPath { get; }
+
+        public Task<T> GetAsync<T>(string prop)
+        {
+            return Properties.ReadProperty<T>(prop);
+        }
+
+        public Task<TV> GetAllAsync()
+        {
+            return Task.FromResult(Properties);
+        }
+
+        public Task SetAsync(string prop, object val)
+        {
+            return Properties.SetProperty(prop, val);
+        }
+
+        public Task<IDisposable> WatchPropertiesAsync(Action<PropertyChanges> handler)
+        {
+            return SignalWatcher.AddAsync(this, nameof(OnPropertiesChanged), handler);
+        }
+
+        public event Action<PropertyChanges> OnPropertiesChanged;
+    }
+}
--- BleCommunication/Infrastructure/BlueZ/Gatt/GattDescriptor.cs
+++ BleCommunication/Infrastructure/BlueZ/Gatt/GattDescriptor.cs
@@ -1,46 +1,18 @@
-﻿using System;
-using System.Threading.Tasks;
-using BleServer.Infrastructure.BlueZ.Utilities;
-using Tmds.DBus;
+﻿using System.Threading.Tasks;
+using BleServer.Infrastructure.BlueZ.Core;
 
 namespace BleServer.Infrastructure.BlueZ.Gatt
 {
-    internal class GattDescriptor : IGattDescriptor1
+    internal class GattDescriptor : PropertiesBase<GattDescriptor1Properties>, IGattDescriptor1
     {
-        private readonly GattDescriptor1Properties _Properties;
-
         public GattDescriptor(GattDescriptor1Properties gattDescriptor1Properties)
+            : base(gattDescriptor1Properties)
         {
-            _Properties = gattDescriptor1Properties;
         }
-
-        public ObjectPath ObjectPath { get; }
 
         public Task<byte[]> ReadValueAsync()
         {
-            return Task.FromResult(_Properties.Value);
+            return Task.FromResult(Properties.Value);
         }
-
-        public Task<T> GetAsync<T>(string prop)
-        {
-            return _Properties.ReadProperty<T>(prop);
-        }
-
-        public Task<GattDescriptor1Properties> GetAllAsync()
-        {
-            return Task.FromResult(_Properties);
-        }
-
-        public Task SetAsync(string prop, object val)
-        {
-            return _Properties.SetProperty(prop, val);
-        }
-
-        public Task<IDisposable> WatchPropertiesAsync(Action<PropertyChanges> handler)
-        {
-            return SignalWatcher.AddAsync(this, nameof(OnPropertiesChanged), handler);
-        }
-
-        public event Action<PropertyChanges> OnPropertiesChanged;
     }
 }
