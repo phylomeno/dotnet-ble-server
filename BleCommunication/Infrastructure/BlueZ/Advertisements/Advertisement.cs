97df6a938fb44e557b378529b3baca397cea3ee7
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-02 06:02:59 +0200

    adjust advertisement to use service uuid

--- BleCommunication/Infrastructure/BlueZ/Advertisements/Advertisement.cs
+++ BleCommunication/Infrastructure/BlueZ/Advertisements/Advertisement.cs
@@ -51,12 +51,12 @@ namespace BleServer.Infrastructure.BlueZ.Advertisements
             var advertisement = new Advertisement(new Dictionary<string, object>
             {
                 {"Type", "peripheral"},
-                {"ServiceUUIDs", new[] {"180D", "180F"}},
+                {"ServiceUUIDs", new[] {"12345678-1234-5678-1234-56789abcdef0" }},
                 // {
                 //     "ManufacturerData",
                 //     new Dictionary<string, object> {{"0xffff", new[] {"0x00", "0x01", "0x02", "0x03"}}}
                 // },
-                {"SolicitUUIDs", new[] {"180D", "180F"}},
+                {"SolicitUUIDs", new[] {"12345678-1234-5678-1234-56789abcdef0"}},
                 // {"ServiceData", new Dictionary<string, object> {{"9999", new[] {"0x00", "0x01", "0x02", "0x03"}}}},
                 {"Includes", new string[0]},
                 {"LocalName", "Edelweiss ASD"},
