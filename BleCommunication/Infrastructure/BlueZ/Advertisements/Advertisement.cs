a5ad4ad48b48625114987cb434795fb9774bf6db
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-03 07:01:14 +0200

    adjust advertisement with correct service id

--- BleCommunication/Infrastructure/BlueZ/Advertisements/Advertisement.cs
+++ BleCommunication/Infrastructure/BlueZ/Advertisements/Advertisement.cs
@@ -51,17 +51,17 @@ namespace BleServer.Infrastructure.BlueZ.Advertisements
             var advertisement = new Advertisement(new Dictionary<string, object>
             {
                 {"Type", "peripheral"},
-                {"ServiceUUIDs", new[] {"12345678-1234-5678-1234-56789abcdef0" }},
+                {"ServiceUUIDs", new[] {"12345678-1234-5678-1234-56789abcdef0"}},
                 // {
                 //     "ManufacturerData",
                 //     new Dictionary<string, object> {{"0xffff", new[] {"0x00", "0x01", "0x02", "0x03"}}}
                 // },
-                {"SolicitUUIDs", new[] {"12345678-1234-5678-1234-56789abcdef0"}},
+                //{"SolicitUUIDs", new[] {"180D", "180F"}},
                 // {"ServiceData", new Dictionary<string, object> {{"9999", new[] {"0x00", "0x01", "0x02", "0x03"}}}},
                 {"Includes", new string[0]},
-                {"LocalName", "Edelweiss ASD"},
+                {"LocalName", "A"},
                 // {"Appearance", 0},
-                {"Duration", (ushort) 10}
+                //{"Duration", (ushort) 10}
                 // {"Timeout", 1000}
             });
 
