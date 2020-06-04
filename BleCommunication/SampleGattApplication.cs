83e571cf9fcb008037bb621e213b01c8fb49cdab
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-04 05:58:37 +0200

    remove obsolete usings

--- BleCommunication/SampleGattApplication.cs
+++ BleCommunication/SampleGattApplication.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using BleServer.Infrastructure.BlueZ;
 using BleServer.Infrastructure.BlueZ.Gatt;
