3172258d481ebcb88d53af908ecf33151fa4f8f9
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-07 11:42:36 +0200

    share server context

--- BleCommunication/Infrastructure/BlueZ/Gatt/GattApplicationManager.cs
+++ BleCommunication/Infrastructure/BlueZ/Gatt/GattApplicationManager.cs
@@ -16,6 +16,28 @@ namespace BleServer.Infrastructure.BlueZ.Gatt
         public async Task RegisterGattApplication(IEnumerable<ServiceDescription> gattServiceDescriptions)
         {
 
+            var application = new GattApplication("/");
+            await connection.RegisterObjectAsync(application);
+
+            var service = new GattService("/org/bluez/example/service0", gattService1Properties);
+            await connection.RegisterObjectAsync(service);
+
+            var characteristic = new GattCharacteristic("/org/bluez/example/service0/characteristic0",
+                gattCharacteristic1Properties);
+            await connection.RegisterObjectAsync(characteristic);
+
+            var descriptor1 = new GattDescriptor("/org/bluez/example/service0/characteristic0/descriptor0",
+                gattDescriptor1Properties);
+            await connection.RegisterObjectAsync(descriptor1);
+
+            characteristic.AddDescriptor(descriptor1);
+
+            service.AddCharacteristic(characteristic);
+
+            application.AddService(service);
+
+            var gattManager = connection.CreateProxy<IGattManager1>("org.bluez", "/org/bluez/hci0");
+            await gattManager.RegisterApplicationAsync(new ObjectPath("/"), new Dictionary<string, object>());
         }
 
 
--- BleCommunication/SampleGattApplication.cs
+++ BleCommunication/SampleGattApplication.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using BleServer.Infrastructure.BlueZ;
+using BleServer.Infrastructure.BlueZ.Core;
 using BleServer.Infrastructure.BlueZ.Gatt;
-using Tmds.DBus;
 
 namespace BleServer
 {
     internal class SampleGattApplication
     {
-        public static async Task RegisterGattApplication(Connection connection)
+        public static async Task RegisterGattApplication(ServerContext serverContext)
         {
             var gattService1Properties = new GattService1Properties
             {
                 UUID = "12345678-1234-5678-1234-56789abcdef0",
-                Characteristics = new[] {new ObjectPath("/org/bluez/example/service0/characteristic0")},
                 Primary = true
             };
 
@@ -23,11 +19,9 @@ namespace BleServer
             {
                 UUID = "12345678-1234-5678-1234-56789abcdef1",
                 Flags = new[] {"read", "write", "writable-auxiliaries"},
-                Service = "/org/bluez/example/service0"
             };
             var gattDescriptor1Properties = new GattDescriptor1Properties
             {
-                Characteristic = "/org/bluez/example/service0/characteristic0",
                 Value = new[] {(byte) 't'},
                 UUID = "12345678-1234-5678-1234-56789abcdef2",
                 Flags = new[] {"read", "write"}
@@ -35,47 +29,9 @@ namespace BleServer
             var gab = new GattApplicationBuilder();
             gab
                 .AddService(gattService1Properties)
-                .WithCharacteristic(gattCharacteristic1Properties, new[] {gattDescriptor1Properties})
+                .WithCharacteristic(gattCharacteristic1Properties, new[] {gattDescriptor1Properties});
 
-            var application = new GattApplication("/");
-            await connection.RegisterObjectAsync(application);
-
-            var service = new GattService("/org/bluez/example/service0", gattService1Properties);
-            await connection.RegisterObjectAsync(service);
-
-            var characteristic = new GattCharacteristic("/org/bluez/example/service0/characteristic0",
-                gattCharacteristic1Properties);
-            await connection.RegisterObjectAsync(characteristic);
-
-            var descriptor1 = new GattDescriptor("/org/bluez/example/service0/characteristic0/descriptor0",
-                gattDescriptor1Properties);
-            await connection.RegisterObjectAsync(descriptor1);
-
-            characteristic.AddDescriptor(descriptor1);
-
-            service.AddCharacteristic(characteristic);
-
-            application.AddService(service);
-
-            var gattManager = connection.CreateProxy<IGattManager1>("org.bluez", "/org/bluez/hci0");
-            await gattManager.RegisterApplicationAsync(new ObjectPath("/"), new Dictionary<string, object>());
-        }
-    }
-
-    internal class GattApplicationBuilder
-    {
-        private readonly IList<GattServiceBuilder> _ServiceBuilders = new List<GattServiceBuilder>();
-
-        public GattServiceBuilder AddService(GattService1Properties gattService1Properties)
-        {
-            var gattServiceBuilder = new GattServiceBuilder();
-            _ServiceBuilders.Add(gattServiceBuilder);
-            return gattServiceBuilder;
-        }
-
-        public IEnumerable<ServiceDescription> BuildServiceDescriptions()
-        {
-            return _ServiceBuilders.Select(s => s.BuildServiceDescription());
+            await new GattApplicationManager(serverContext).RegisterGattApplication(gab.BuildServiceDescriptions());
         }
     }
 }
