c79524b4d952662572b70021b0d2398945280149
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-08 06:15:59 +0200

    introduce example project

---
+++ Examples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BleServer
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello Bluetooth");
+
+            Task.Run(async () =>
+            {
+                using (var connection = new Connection(Address.System))
+                {
+                    await ConnectToDBus(connection);
+                    var serverContext = new ServerContext();
+                    await SampleAdvertisement.RegisterSampleAdvertisement(serverContext);
+                    await SampleGattApplication.RegisterGattApplication(serverContext);
+
+                    Console.WriteLine("Press CTRL+C to quit");
+                    await Task.Delay(-1);
+                }
+            }).Wait();
+        }
+
+        private static async Task ConnectToDBus(IConnection connection)
+        {
+            await connection.ConnectAsync();
+        }
+    }
+}
---
+++ Examples/SampleGattApplication.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using BleServer.Infrastructure.BlueZ;
+using BleServer.Infrastructure.BlueZ.Core;
+using BleServer.Infrastructure.BlueZ.Gatt;
+
+namespace BleServer
+{
+    internal class SampleGattApplication
+    {
+        public static async Task RegisterGattApplication(ServerContext serverContext)
+        {
+            var gattService1Properties = new GattService1Properties
+            {
+                UUID = "12345678-1234-5678-1234-56789abcdef0",
+                Primary = true
+            };
+
+            var gattCharacteristic1Properties = new GattCharacteristic1Properties
+            {
+                UUID = "12345678-1234-5678-1234-56789abcdef1",
+                Flags = new[] {"read", "write", "writable-auxiliaries"},
+            };
+            var gattDescriptor1Properties = new GattDescriptor1Properties
+            {
+                Value = new[] {(byte) 't'},
+                UUID = "12345678-1234-5678-1234-56789abcdef2",
+                Flags = new[] {"read", "write"}
+            };
+            var gab = new GattApplicationBuilder();
+            gab
+                .AddService(gattService1Properties)
+                .WithCharacteristic(gattCharacteristic1Properties, new[] {gattDescriptor1Properties});
+
+            await new GattApplicationManager(serverContext).RegisterGattApplication(gab.BuildServiceDescriptions());
+        }
+    }
+}
