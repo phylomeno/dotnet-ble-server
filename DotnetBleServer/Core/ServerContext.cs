746df9c31f92560174495d380128b560711c3881
Lukas Kamber <lukas.kamber@securiton.ch>
2020-06-15 19:29:43 +0200

    logging example

--- DotnetBleServer/Core/ServerContext.cs
+++ DotnetBleServer/Core/ServerContext.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
 using Tmds.DBus;
 
 namespace DotnetBleServer.Core
 {
     public class ServerContext : IDisposable
     {
-        public ServerContext()
+        public ServerContext(ILogger logger = null)
         {
+            logger = logger ?? NullLogger.Instance;
+
+            logger.LogWarning("system address = {SystemAddress}", Address.System);
             Connection = new Connection(Address.System);
         }
 
--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
+
 using DotnetBleServer.Core;
+
+using Microsoft.Extensions.Logging;
 
 namespace Examples
 {
@@ -8,21 +11,27 @@ namespace Examples
     {
         private static void Main()
         {
+            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            var logger = loggerFactory.CreateLogger("BLEasy");
+            logger.LogInformation("console logging");
+
             Console.WriteLine("Hello Bluetooth");
             Console.ReadKey();
 
-            Task.Run(async () =>
-            {
-                using (var serverContext = new ServerContext())
-                {
-                    await serverContext.Connect();
-                    await SampleAdvertisement.RegisterSampleAdvertisement(serverContext);
-                    await SampleGattApplication.RegisterGattApplication(serverContext);
+            Task.Run(
+                    async () =>
+                    {
+                        using (var serverContext = new ServerContext(logger))
+                        {
+                            await serverContext.Connect();
+                            await SampleAdvertisement.RegisterSampleAdvertisement(serverContext);
+                            await SampleGattApplication.RegisterGattApplication(serverContext);
 
-                    Console.WriteLine("Press CTRL+C to quit");
-                    await Task.Delay(-1);
-                }
-            }).Wait();
+                            Console.WriteLine("Press CTRL+C to quit");
+                            await Task.Delay(-1);
+                        }
+                    })
+                .Wait();
         }
     }
 }
