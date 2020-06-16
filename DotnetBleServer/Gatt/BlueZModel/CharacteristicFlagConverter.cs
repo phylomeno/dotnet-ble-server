607ec3aa32a7f6249b26244a5c59a71af372d045
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-16 05:46:16 +0200

    use flag converter

---
+++ DotnetBleServer/Gatt/BlueZModel/CharacteristicFlagConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using DotnetBleServer.Gatt.Description;
+
+namespace DotnetBleServer.Gatt.BlueZModel
+{
+    internal class CharacteristicFlagConverter
+    {
+        private static readonly Dictionary<CharacteristicFlags, string> FlagMappings =
+            new Dictionary<CharacteristicFlags, string>
+            {
+                {CharacteristicFlags.Read, "read"},
+                {CharacteristicFlags.Write, "write"},
+                {CharacteristicFlags.WritableAuxiliaries, "writable-auxiliaries"}
+            };
+
+        public static string[] ConvertFlags(CharacteristicFlags characteristicFlags)
+        {
+            return (from mapping in FlagMappings where (characteristicFlags & mapping.Key) > 0 select mapping.Value).ToArray();
+        }
+    }
+}
